68bf6fe9ebb4e94b3d1a48c9237a14d1b601bea5
Salmandotweb <salmandotweb@gmail.com>
2023-06-25 12:11:49 +0500

    refactoring

--- src/components/user-auth-form.tsx
+++ src/components/user-auth-form.tsx
@@ -8,25 +8,22 @@ import { Icons } from "./icons";
 export function UserAuthForm({ providers }: any) {
   const googleProvider = providers.google;
 
-  console.log(providers);
-
   return (
     <>
-      {Object.values(providers).map((provider: any) => {
-        <Button
-          variant="outline"
-          key={googleProvider.name}
-          className="flex items-center justify-center rounded-lg border py-2.5 duration-150 hover:bg-gray-50 active:bg-gray-100"
-          onClick={() => {
-            signIn(googleProvider.id, {
-              callbackUrl: "/",
-            }).catch(console.error);
-          }}
-        >
-          <Icons.google className="mr-2 h-4 w-4" />
-          Google
-        </Button>;
-      })}
+      <Button
+        variant="outline"
+        key={googleProvider.name}
+        className="flex items-center justify-center rounded-lg border py-2.5 duration-150 hover:bg-gray-50 active:bg-gray-100"
+        onClick={() => {
+          signIn(googleProvider.id, {
+            callbackUrl: "/",
+          }).catch(console.error);
+        }}
+      >
+        <Icons.google className="mr-2 h-4 w-4" />
+        Google
+      </Button>
+      ;
     </>
   );
 }
